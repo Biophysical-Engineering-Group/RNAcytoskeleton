602bd3b6f991b68b96179cab516dcc5406eaf33f
Mai P Tran <92101220+maiptran21@users.noreply.github.com>
2025-01-09 13:13:50 +0700

    Add files via upload

---
+++ Figure 3/simulation/preprocess/run_ispi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+# Job name
+#SBATCH -J T3H
+
+# How many resources to use
+#SBATCH -N 1
+#SBATCH -n 10
+#SBATCH -c 1
+#SBATCH --constraint="gpu"
+#SBATCH --gres=gpu:a100:1
+#SBATCH --mem=40000
+#SBATCH --time=1-00:00:00
+
+conda activate oxDNA
+
+python ~/final_scripts/longfil_process.py trajectory.dat filament.top 3 --remove 0.1 --start_positions 58 --end_positions 315
